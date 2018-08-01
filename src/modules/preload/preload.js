1f449f7518cdad230f4eec437061a63cd11710a9
Adam  Phillips <aphillips@squiz.net>
2018-08-01 18:38:12 +1000

    Set up some default animations for show off

--- src/modules/preload/preload.js
+++ src/modules/preload/preload.js
@@ -3,25 +3,32 @@ import anime from 'animejs'
 const $preload = document.querySelector('.preload');
 const $preloadTitle = $preload.querySelector('.preload__title');
 
-function splitTitle(title) {
+const splitTitle = (title) => {
     const text = title.textContent;
-    title.innerHTML = text.replace(/([^\x00-\x80]|\w)/g, "<div class='preload__title-letter' style='position:relative;display:inline-block;'>$&</div>");
+    title.innerHTML = text.replace(/([^\x00-\x80]|\w)/g, "<div class='preload__title-letter' style='position:relative;display:inline-block;'><div style='position:relative;display:inline-block;'>$&</div></div>");
     animateIn();
 }
 
-function animateIn() {
+const animateIn = () => {
     anime({
-        targets: '.preload__title-letter',
-        translateY: [0,-100],
-        opacity: [1,0],
-        easing: "easeInExpo",
-        duration: 1200,
+        targets: '.preload__title-letter div',
+        //translateX: -100,
+        opacity: 0,
+        duration: 500,
+        easing: 'easeInQuad',
         delay: function(el, i) {
             return 100 + 30 * i;
+        },
+        complete:() => {
+            anime({
+                targets: '.preload__block-background',
+                height: 0,
+                duration: 500,
+                easing: 'easeInQuad'
+            });
         }
     });
 }
-
 
 document.onreadystatechange = function () {
     console.log('page loaded')
