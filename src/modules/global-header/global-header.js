6738e36503cd9a9d28f3074c1314cebcd7fd3905
Adam  Phillips <aphillips@squiz.net>
2018-08-23 09:30:53 +1000

    Mobil nav redone

--- src/modules/global-header/global-header.js
+++ src/modules/global-header/global-header.js
@@ -1,36 +1,25 @@
 import anime from 'animejs'
 
-// Set up Canvas
-const c = document.getElementById("menuc");
-const ctx = c.getContext("2d");
 const $menuOpen = document.querySelector('.header__nav-mobile__open');
+const $menuClose = document.querySelector('.header__nav-mobile__close');
+const $nav = document.querySelector('.header__nav')
 
-const resizeCanvas = () => {
-    let cW = window.innerWidth;
-    let cH = window.innerHeight;
-    c.width = cW * devicePixelRatio;
-    c.height = cH * devicePixelRatio;
-    ctx.scale(devicePixelRatio, devicePixelRatio);
+function addClickListeners() {
+    $menuOpen.addEventListener("touchstart", openMenu);
+    $menuOpen.addEventListener("mousedown", openMenu);
+
+    $menuClose.addEventListener("touchstart", closeMenu);
+    $menuClose.addEventListener("mousedown", closeMenu);
 };
 
-
-const starterCircle = () => {
-    
-    var centerX = $menuOpen.getBoundingClientRect().left + $menuOpen.getBoundingClientRect().width / 2;
-    var centerY = $menuOpen.getBoundingClientRect().top + $menuOpen.getBoundingClientRect().height / 2;
-    var radius = 10;
-    
-    console.log(centerX, centerY);
-	// define the arc path
-	ctx.beginPath();
-	ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-	ctx.fillStyle = 'rgba(125,0,0,1)';
-    ctx.fill();
-    ctx.save();
-    console.log('circle drawn')
+function openMenu(e) {
+    $nav.classList.add('active')
 }
 
-(global => {
-    resizeCanvas();
-    starterCircle();
-})(window)
+function closeMenu(e) {
+    $nav.classList.remove('active')
+}
+
+(function init() {
+    addClickListeners();
+})();
