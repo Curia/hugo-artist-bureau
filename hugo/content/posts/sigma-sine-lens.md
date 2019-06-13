ae614b19fe5e225fddeabba93234b201d29e9430
Adam  Phillips <aphillips@squiz.net>
2019-06-13 22:45:53 +1000

    Slide positioning

--- hugo/content/posts/sigma-sine-lens.md
+++ hugo/content/posts/sigma-sine-lens.md
@@ -2,7 +2,7 @@
 client_name = "Sigma"
 date = "2018-09-19T07:29:45+00:00"
 draft = true
-media_type = "Youtube"
+media_type = "Video"
 portfolio_thumbnail = "/uploads/2018/09/19/portfolio-1.jpg"
 title = "Sigma Cine Lens"
 
--- src/modules/artist-slide/artist-slide.js
+++ src/modules/artist-slide/artist-slide.js
@@ -2,16 +2,18 @@ import fullpage from 'fullpage.js';
 
 const offsetContent = () => {
     const $header = document.getElementById('header').clientHeight;
-    const $contentBody =  document.getElementById('main');
-    $contentBody.style.marginTop = `${$header}px`;
+    const $contentBody =  document.getElementById('body');
+    $contentBody.style.paddingTop = `${$header}px`;
 }
+
+const pageMargin = () => (`${document.getElementById('header').clientHeight}px`);
 
 const fullPageInstance = new fullpage('.fullpage', {
     navigation: true,
+    verticalCentered: false,
+    css3: true,
+    paddingTop: pageMargin(),
     afterResize: function(width, height){
-        offsetContent(); 
-    },
-    afterRender: function(){
-        offsetContent(); 
+        fullpage_api.reBuild();
     }
 });
