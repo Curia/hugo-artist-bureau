fdb50b155bfa0e82ddcee3cb40a2e9b10a44a8fb
Adam  Phillips <aphillips@squiz.net>
2019-06-16 14:53:14 +1000

    Added youtube functionality

--- src/modules/artist-slide/artist-slide.js
+++ src/modules/artist-slide/artist-slide.js
@@ -1,19 +1,39 @@
 import fullpage from 'fullpage.js';
+import YouTubePlayer from 'youtube-player';
 
-const offsetContent = () => {
-    const $header = document.getElementById('header').clientHeight;
-    const $contentBody =  document.getElementById('body');
-    $contentBody.style.paddingTop = `${$header}px`;
+const setEvents = () => {
+    const slideBackground = document.getElementsByClassName('artist-slide__background');
+
+    for (let i = 0; i < slideBackground.length; i++) {
+        const slide = slideBackground[i];
+        const button = slide.getElementsByClassName('slide-background__play')[0];
+        const video = slide.getElementsByClassName('slide-background__video')[0];
+        const youtubeId = button.getAttribute('data-youtube');
+
+        button.addEventListener('click', () =>{
+            button.classList.add('active');
+            loadVideos(youtubeId, video);
+        })
+    }
+}
+
+const loadVideos = (id, target) => {
+    let player = YouTubePlayer(target);
+    
+    target.classList.add('active');
+    player.loadVideoById(id);
 }
 
 const pageMargin = () => (`${document.getElementById('header').clientHeight}px`);
 
 const fullPageInstance = new fullpage('.fullpage', {
-    navigation: true,
     verticalCentered: false,
     css3: true,
     paddingTop: pageMargin(),
+    autoScrolling: false,
     afterResize: function(width, height){
         fullpage_api.reBuild();
     }
 });
+
+setEvents();
