51c94b2a9062a3625529515e83bdafff4c146815
Adam  Phillips <aphillips@squiz.net>
2019-07-17 08:50:01 +1000

    Better way to set up slider styling

--- src/modules/artist-slide/artist-slide.js
+++ src/modules/artist-slide/artist-slide.js
@@ -15,32 +15,6 @@ const buildSlider = () => {
     });
 }
 
-const isPortrait = (img) => {
-    var w = img.naturalWidth || img.width,
-        h = img.naturalHeight || img.height;
-    return (h > w);
-}
-
-const setSliderHeight = () => {
-    const imgArr = document.querySelectorAll('.tns-lazy-img');
-    const slideBody = document.querySelector('.artist-slide__body').clientHeight;
-    const slideTitle = document.querySelector('.artist-slide__heading').clientHeight;
-
-    for (let i = 0; i < imgArr.length; i++) {
-        const img = imgArr[i];
-
-        isPortrait(img) ? (
-            //img.style.height = `${slideBody - slideTitle - 30}px`
-            img.style.height = 'auto',
-            img.style.width = '100%'
-        ) : (
-            img.style.height = 'auto',
-            img.style.width = '100%'
-        )
-    }
-    buildSlider();
-}
-
 const fullPageInstance = new fullpage('.fullpage', {
     verticalCentered: false,
     css3: true,
@@ -49,7 +23,8 @@ const fullPageInstance = new fullpage('.fullpage', {
     scrollOverflow: true,
     autoScrolling: false,
     afterLoad: function(origin, destination, direction){
-        setSliderHeight();
+        //setSliderHeight();
+        buildSlider();
     },
     afterResize: function(width, height){
         fullpage_api.reBuild();
